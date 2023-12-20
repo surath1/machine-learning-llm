b7fe2a6b3b75a1bfbf53bab9ed4ad47a229befa0
surath1 <srout12321@gmail.com>
2023-12-20 17:36:42 -0500

    folder strecture created for llm model 01

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "machine-learning-llm"
+AUTHOR_USER_NAME = "surath1"
+SRC_REPO = "logchain_model"
+AUTHOR_EMAIL = "xyz@gmail.com"
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="python package for LLM",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Issue Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
